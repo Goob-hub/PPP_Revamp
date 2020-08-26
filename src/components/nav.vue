86eae7374cf30c984ec2559fdbd96947063f8086
GJS2002 <67513182+GJS2002@users.noreply.github.com>
2020-08-25 19:09:14 -0500

    Added all files

---
+++ src/components/nav.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav>
+       <header>
+          <p>logo</p>
+          <h1>PPP</h1>
+          <navBtn />
+       </header>
+       <main>
+            <button class="btn">home</button>
+            <button class="btn">about</button>
+            <button class="btn">games</button>
+            <button class="btn">devblog</button>
+       </main>
+  </nav>
+</template>
+
+<script>
+import navBtn from '@/components/burgerBtn.vue';
+export default {
+     name: 'navBar',
+     components: {
+          navBtn
+     }
+
+}
+</script>
+
+<style lang="scss">
+nav{
+     display: flex;
+     flex-direction: column;
+     height: 15vh;
+     overflow: hidden;
+     background: var(--clrBackdrop);
+     transition: .5s linear;
+
+     header{
+          display: flex;
+          justify-content: space-evenly;
+          align-items: center;
+          width: 100%;
+          min-height: 15vh;
+     }
+
+     main{
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+          flex-direction: column;
+          padding: 0 30% 30% 30%;
+          min-height: 85vh;
+     }
+
+     img{
+          height: 3em;
+          width: 5em;
+     }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <navBar />
+  </div>
+</template>
+
+<script>
+import navBar from '@/components/nav.vue' 
+
+export default {
+  name: 'Home',
+  components: {
+    navBar
+  }
+}
+</script>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: var(--clrMain);
+}
+
+.btn{
+  background: none;
+  border: none;
+  width: max-content;
+  height: max-content;
+  &:focus{
+    outline: none;
+  }
+}
+
+.home{
+  background: black;
+  min-height: 100vh;
+  max-width: 100vw;
+}
+</style>
